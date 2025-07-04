725981f2501a09ad9178a02f96da0d6bed66afb0
clmoralesl <cl.moralesl@duocuc.cl>
2025-07-04 18:53:49 -0400

    Se ajusta página de login y se añade codificador a UTF-8 para evitar problemas con carácteres

--- src/components/axiosInstance.js
+++ src/components/axiosInstance.js
@@ -16,4 +16,37 @@ axiosInstance.interceptors.request.use(
   (error) => Promise.reject(error)
 );
 
+// En tu archivo axiosInstance.js
+function decodeTexto(texto) {
+  try {
+    return typeof texto === 'string' ? decodeURIComponent(escape(texto)) : texto;
+  } catch {
+    return texto;
+  }
+}
+
+function decodeObject(obj) {
+  if (Array.isArray(obj)) {
+    return obj.map(decodeObject);
+  } else if (obj && typeof obj === 'object') {
+    const res = {};
+    for (const key in obj) {
+      res[key] = decodeObject(obj[key]);
+    }
+    return res;
+  } else {
+    return decodeTexto(obj);
+  }
+}
+
+axiosInstance.interceptors.response.use(
+  (response) => {
+    if (response.data) {
+      response.data = decodeObject(response.data);
+    }
+    return response;
+  },
+  (error) => Promise.reject(error)
+);
+
 export default axiosInstance;
